4f89b6e5ed4558eae1327fd029ac259f286e82c1
rpattem <rajesh@taskvalue.in>
2024-03-26 17:26:21 +0530

    addCors

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 const users = new Map();
 
 // Socket.io
